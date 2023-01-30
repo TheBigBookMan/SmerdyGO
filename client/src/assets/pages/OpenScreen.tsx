1d4f072f9a8a1248bc0d943836517e35c849616c
Ben Smerd <bsmerd@adam.com.au>
2023-01-30 10:34:10 +1030

    created new OpenScreen component for when users open the application and first screen dependent on if logged in or not

---
+++ client/src/assets/pages/OpenScreen.tsx
@@ -0,0 +1,35 @@
+import UseUserContext from "../context/UserContext";
+import Header from "../components/common/Header/Header";
+import SideBar from "../components/common/SideBar/SideBar";
+import Pages from "./Pages";
+import Login from "./Login";
+import Signup from "./Signup";
+import { Routes, Route } from "react-router-dom";
+
+const OpenScreen = () => {
+  const { isLoggedIn } = UseUserContext();
+  console.log(isLoggedIn);
+
+  return (
+    <div className="font-mono w-screen h-screen+">
+      <>
+        {isLoggedIn ? (
+          <div className="flex">
+            <SideBar />
+            <div className="flex flex-col w-full">
+              <Header />
+              <Pages />
+            </div>
+          </div>
+        ) : (
+          <Routes>
+            <Route index path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        )}
+      </>
+    </div>
+  );
+};
+
+export default OpenScreen;
