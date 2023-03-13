366a23ba0cd2e4eff0a594d2f5ed214cefbcb876
Ben Smerd <bsmerd@adam.com.au>
2023-03-13 11:55:22 +1030

    fix the loading of new completed subgoals into goals

--- client/src/assets/components/features/Goals/ProgressBarComp.tsx
+++ client/src/assets/components/features/Goals/ProgressBarComp.tsx
@@ -13,32 +13,26 @@ const ProgressBarComp = ({
   const goalToCompleteAmount = selectedGoal?.amountCompleted;
   const updatedAmount =
     updatedGoalCompletedAmount?.completeSubGoal.amountCompleted;
-  console.log(updatedAmount);
-  const [percentGoalAmountCompleted, setPercentGoalAmountCompleted] = useState<
-    string | null
-  >();
-  //todo set the amount completed based off the selected goal amount (generated from querying database) and then have a usestate data change call for the updatedGoalCompletedAmount coming in from updating the subgoal completion
-  //todo need a function that calculates the percent of the progress done by the goal amount
-  console.log(selectedGoal);
-  console.log(updatedGoalCompletedAmount);
+  const [percentGoalAmountCompleted, setPercentGoalAmountCompleted] =
+    useState<string>();
 
   useEffect(() => {
     if (goalToCompleteAmount) {
-      console.log("HEREE");
       const percentCompleted = (goalToCompleteAmount / goalAmount) * 100;
       const twoDecimal = percentCompleted.toFixed(2);
       const stringPercent = twoDecimal.toString();
       setPercentGoalAmountCompleted(stringPercent);
     }
-    if (updatedAmount !== goalToCompleteAmount) {
+    if (
+      typeof updatedAmount === "number" &&
+      updatedAmount !== goalToCompleteAmount
+    ) {
       const percentCompleted = (updatedAmount / goalAmount) * 100;
       const twoDecimal = percentCompleted.toFixed(2);
       const stringPercent = twoDecimal.toString();
       setPercentGoalAmountCompleted(stringPercent);
     }
   }, [updatedGoalCompletedAmount, selectedGoal]);
-
-  console.log(percentGoalAmountCompleted);
 
   return (
     <div>
